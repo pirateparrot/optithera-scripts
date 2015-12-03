2db085b5aec94799c71188cd6593e938ee9f2e05
SGrondin <github@simongrondin.name>
2015-12-03 01:02:51 -0500

    First multi-file table

--- advance.py
+++ advance.py
@@ -1,11 +1,25 @@
 import psycopg2
 import utils
 
+output_dir = "./output/"
+
 conn = psycopg2.connect("dbname={0} host={1} user={0} password={2}".format("postgres", "127.0.0.1", "testtest"))
 cursor = conn.cursor()
 
+# Individuals_#####_Phenotypes
+cursor.execute("""SELECT * FROM "public"."phenotype";""")
+f = cursor.fetchone()
+while f:
+	print f
+	with open(output_dir + "Individuals_" + str(f[0]) + "_Phenotypes.tsv", "w") as file:
+		file.write(utils.to_csv(f))
+	f = cursor.fetchone()
+
+
+# Batches
 cursor.execute("""SELECT * FROM "public"."batch";""")
-print utils.drain_cursor_to_csv(cursor)
+with open(output_dir + "Batches.tsv", "w") as file:
+	file.write(utils.drain_cursor_to_csv(cursor))
 
 
-print "hello world"
+print "Done"
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
+def prepare_fields(li):
+	for i, x in enumerate(li):
+		updated = str(x)
+		li[i] = updated if "\n" not in updated else "\"" + updated + "\""
+	return li
+
 def to_csv(li):
-	return ",".join(map(str, li))
+	return ",".join(prepare_fields(list(li)))
 
 def drain_cursor_to_csv(cursor):
 	result = []
