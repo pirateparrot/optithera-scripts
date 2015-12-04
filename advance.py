6d783abaf208189a74a160c87cb3258bca787e28
SGrondin <github@simongrondin.name>
2015-12-03 23:47:18 -0500

    refactoring, progress on phenotypes

--- advance.py
+++ advance.py
@@ -7,30 +7,42 @@ conn = psycopg2.connect("dbname={0} host={1} user={0} password={2}".format("post
 cursor = conn.cursor()
 
 ### Individuals_#####_Phenotypes ###
-cursor.execute("""SELECT * FROM "public"."phenotype";""")
+cursor.execute("""
+	SELECT V.person_id, V.id, P.name, M.value, P.um, P.descr, V.visit_date
+	FROM "public"."phenotype" AS P
+	LEFT JOIN (
+		SELECT DISTINCT ON (phenotype_id) * FROM "public"."measure" M
+	) AS M ON P.id = M.phenotype_id
+	LEFT JOIN "public"."visit" V ON M.visit_id = V.id
+	;
+""")
 f = cursor.fetchone()
 while f:
 	with open(output_dir + "Individuals_" + str(f[0]) + "_Phenotypes.tsv", "w") as file:
-		file.write(utils.to_csv(f))
+		file.write("visitId,phenotypeType,phenotypeGroup,name,measureDataType,measure,units,description,diagnosisDate\n")
+		li = utils.to_prepared_list(f)
+		file.write("""{1},,,{2},,{3},{4},{5},{6}""".format(*li))
 	f = cursor.fetchone()
 
 
 ### Batches ###
 with open(output_dir + "Batches.tsv", "w") as file:
-	file.write("batchId,batchDate,description,studyName\n")
+	file.write("batchId,batchDate,batchType,description,studyName\n")
 
 	# First do the batch table
-	cursor.execute("""SELECT * FROM "public"."batch";""")
+	cursor.execute("""SELECT B.id, B.batch_date, B.description FROM "public"."batch" AS B;""")
 	f = cursor.fetchone()
 	while f:
-		file.write(utils.to_csv(f) + ",Genotyping Batch\n")
+		li = utils.to_prepared_list(f)
+		file.write("""{0},{1},ADVANCE-ON,{2},Genotyping Batch\n""".format(*li))
 		f = cursor.fetchone()
 
 	# Then do the samples table
-	cursor.execute("""SELECT * FROM "public"."samples";""")
+	cursor.execute("""SELECT B.id, B.sample_date, B.description FROM "public"."samples" AS B;""")
 	f = cursor.fetchone()
 	while f:
-		file.write(utils.to_csv(f) + ",Enrollment Batch\n")
+		li = utils.to_prepared_list(f)
+		file.write("""{0},{1},ADVANCE-ON,{2},Enrollment Batch\n""".format(*li))
 		f = cursor.fetchone()
 
 
--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 def prepare_fields(li):
 	for i, x in enumerate(li):
-		updated = str(x)
+		updated = str(x) if x is not None else ""
 		li[i] = updated if "\n" not in updated else "\"" + updated + "\""
 	return li
 
+def to_prepared_list(li):
+	return prepare_fields(list(li))
+
 def to_csv(li):
-	return ",".join(prepare_fields(list(li)))
+	return ",".join(to_prepared_list(li))
 
 def drain_cursor_to_csv(cursor):
 	result = []
