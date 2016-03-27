65e649587c262d7a812fa36ba5ab38f5f8220b88
SGrondin <github@simongrondin.name>
2016-03-27 13:38:52 -0400

    Write to parquet

--- src/main/scala/import.scala
+++ src/main/scala/import.scala
@@ -5,13 +5,25 @@ import scala.collection.JavaConversions._
 import org.bdgenomics.formats.avro._
 
 class IndividualRecord {
-  var individual: Option[Individual] = _
-  var visits: List[Visit] = _
-  var phenotypes: List[Phenotype] = _
-  var genotypes: collection.mutable.HashMap[String,Genotype] = _
-  def isReady(nbGenotypes : Int) =
+  var individual: Option[Individual.Builder] = _
+  var visits: List[Visit.Builder] = _
+  var phenotypes: List[Phenotype.Builder] = _
+  var genotypes: List[Genotype.Builder] = _
+  def isReady(nbGenotypes : Int) = {
     !(this.individual.isEmpty || this.visits.length == 0 ||
     this.phenotypes.length == 0 || this.genotypes.size < nbGenotypes)
+  }
+  def toWritable() = {
+    val listPhenotypes = this.phenotypes.map { p => p.build() }
+    val listGenotypes = this.genotypes.map { g => g.build() }
+    val listVisits = this.visits.map { visit =>
+      visit.setPhenotypes(listPhenotypes)
+      visit.build()
+    }
+    this.individual.get.setVisits(listVisits)
+    this.individual.get.setGenotypes(listGenotypes)
+    this.individual.get.build
+  }
 }
 
 object IndividualRecord {
@@ -20,7 +32,7 @@ object IndividualRecord {
     ind.individual = None
     ind.visits = List()
     ind.phenotypes = List()
-    ind.genotypes = new collection.mutable.HashMap[String,Genotype]
+    ind.genotypes = List()
     ind
   }
 }
@@ -33,19 +45,37 @@ object Importer {
       case _ : Throwable =>
     }
 
-  def main(args: Array[String]) =
-    println("Hello World!!")
+    // It blows my mind that this isn't part of the standard library
+    def optionListToList[T](list: List[Option[T]]) : List[T] = {
+      list.filter(x => !x.isEmpty).map(x => x.get)
+    }
+
+  def main(args: Array[String]) = {
 
     val individualsById = new collection.mutable.HashMap[String,IndividualRecord]
     val dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd")
     val prefix = "./output/"
+    val NB_INDIVIDUAL_COLUMNS = 11
     val filePattern = """^Individuals_([0-9]+)(_(.*))?\.tsv$""".r
     val genotypePattern = """^Individuals_([0-9]+)_(v[0-9]+)_Genotypes\.tsv$""".r
     var nbGenotypeBatches : Option[Int] = None
+    val batchData = new collection.mutable.HashMap[String,Batch]()
+    var writeToParquet = scala.collection.mutable.Buffer[Individual]()
+    val parquetWriter = new ParquetWriter[Individual](writeToParquet, "./individuals", Individual.getClassSchema)
 
     val list = new java.io.File(prefix).listFiles.map(file => file.getName).sorted
-    list.foreach(fileName =>
-      filePattern.findAllIn(fileName).matchData foreach { m =>
+    list.foreach({ fileName =>
+      if (fileName == "Batches.tsv") {
+        val reader = new CSVReader(new FileReader(prefix + fileName))
+        reader.readNext() // Skip headers
+        reader.readAll.foreach { row =>
+          val batch = Batch.newBuilder()
+          batch.setBatchDateEpoch(dateFormat.parse(row(1)).getTime)
+          batch.setBatchType(row(2))
+          batch.setDescription(row(3))
+          batchData += row(0) -> batch.build()
+        }
+      } else filePattern.findAllIn(fileName).matchData foreach { m =>
 
         val current = individualsById.get(m.group(1)).getOrElse(IndividualRecord())
         val reader = new CSVReader(new FileReader(prefix + fileName))
@@ -60,8 +90,7 @@ object Importer {
               visit.setStudyId(row(2))
               visit.setIsFasting(Try(java.lang.Boolean.valueOf(row(3))).getOrElse(false))
               visit.setDescription(row(4))
-              // visit.setPhenotypes(phenotypes) // TODO
-              current.visits = visit.build() :: current.visits
+              current.visits = visit :: current.visits
             }
           case (id, "Phenotypes") =>
             reader.readAll.foreach { row =>
@@ -74,18 +103,17 @@ object Importer {
               phenotype.setMeasureUnits(row(6))
               phenotype.setDescription(row(7))
               ignore(() => phenotype.setDiagnosisDateEpoch(dateFormat.parse(row(8)).getTime))
-              current.phenotypes = phenotype.build() :: current.phenotypes
+              current.phenotypes = phenotype :: current.phenotypes
             }
           case (id, null) =>
             reader.readAll.foreach { row =>
 
               // Set the nb of genotypes the first time
               if (nbGenotypeBatches.isEmpty)
-                nbGenotypeBatches = Some(row.length - 11) // There are 11 normal columns
+                nbGenotypeBatches = Some(row.length - NB_INDIVIDUAL_COLUMNS)
 
               val gender = if (row(5).charAt(0) == 'm') Gender.Male else if (row(5).charAt(0) == 'f') Gender.Female else Gender.Unknown
               val individual = Individual.newBuilder()
-              // individual.setGenotypes(genotypes) // TODO
               individual.setGender(gender)
               individual.setIndividualId(row(0))
               individual.setFamilyId(row(1))
@@ -97,35 +125,33 @@ object Importer {
               individual.setRegion(row(8))
               individual.setCountry(row(9))
               individual.setNotes(row(10))
-              // individual.setVisits(visits) // TODO
-              // individual.setBatches(batches) // TODO
-              current.individual = Some(individual.build())
+              val listBatches = row.toList.drop(NB_INDIVIDUAL_COLUMNS).map({batchId =>
+                if (batchId == "None") None else batchData.get(batchId)
+              })
+              individual.setBatches(optionListToList(listBatches))
+              current.individual = Some(individual)
             }
           case (id, _) =>
             genotypePattern.findAllIn(fileName).matchData foreach { n =>
-              val version = n.group(2)
+              // This might be useful in the future:
+              // val version = n.group(2)
               val genotype = Genotype.newBuilder()
-              current.genotypes += version -> genotype.build()
+              current.genotypes = genotype :: current.genotypes
             }
           case _ => throw new Exception("Invalid file name: " + fileName)
         }
 
-        if (current.isReady(nbGenotypeBatches.getOrElse(999)))
+        if (current.isReady(nbGenotypeBatches.getOrElse(999))) {
           println("Ready!")
+          writeToParquet.append(current.toWritable())
+        }
 
         individualsById += m.group(1) -> current
       }
-    )
+    })
 
+    // Persist
+    parquetWriter.persistData
 
-
-  //     val batches = reader.readAll.map { row =>
-  //       Batch.newBuilder()
-  //         .setBatchDateEpoch(dateFormat.parse(row(1)).getTime)
-  //         .setBatchType(row(2))
-  //         .setDescription(row(3))
-  //         .build()
-  //     }
-
-
+  }
 }
