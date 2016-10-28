c889a80bda7edac1fcfbf83eae29dd1f6f0d1222
pirateparrot <diemalvarez@gmail.com>
2016-10-27 22:19:27 -0400

    Created/Modified scripts for IMPUTE2 ,gen files
    1- Created gentotsv.py script. This script takes .gen and .sample file and convert them to new IND, SAM and SNP .tsv files.
    2- Created config.py script. Has some default values in case of running scripts without parameters.
    3- Modified utils.py script. Added new shared functions used by the others scripts.
    4- Modified export.py. Integrate Advance, DBNSP and .gen.sample data.

--- export.py
+++ export.py
@@ -3,19 +3,46 @@ import os
 from time import sleep
 import psycopg2
 import utils
+import config
+import re
 
+
+ 
 # Constants
 # utils.NEWLINE_REPLACEMENT = " " # Can be uncommented and edited
 # utils.SUBARRAY_SEPARATOR = ";" # Can be uncommented and edited
 
-if len(sys.argv) != 6:
+
+#if len(sys.argv) != 6:
+if len(sys.argv) != 7:
 	print "Passed arguments were: " + str(sys.argv)
-	print "Arguments must be: export.py outputDir dbHost dbName dbUser dbPassword"
+	#print "Arguments must be: export.py outputDir dbHost dbName dbUser dbPassword"
+	print "Arguments must be: export.py outputDir dbHost dbName dbUser dbPassword importGenotypeFlag"
 	exit(0)
 
+
+logger=utils.create_logger("export") 
+
 output_dir = "./" + sys.argv[1]
+print output_dir
 output_dir = output_dir if output_dir[-1] == "/" else output_dir + "/"
 connection_string = "dbname={1} host={0} user={2} password={3}".format(*sys.argv[2:])
+
+importGenotypesFlag = None
+sourcesDirectory = None
+
+
+importGenotypesFlag = sys.argv[6]
+sourcesDirectory = config.source_converted_oxford
+file_type_oxford = config.destination_file_type_oxford
+
+if sourcesDirectory[-1]=="/":
+   sourcesDirectory = sourcesDirectory
+else: 
+   sourcesDirectory=sourcesDirectory + "/"
+   sourcesFileList = sorted(os.listdir(sourcesDirectory))
+	
+	
 
 print "Connecting to " + connection_string
 conn = psycopg2.connect(connection_string)
@@ -31,19 +58,12 @@ print "Exporting Individuals"
 
 # array_agg + array_to_string = Like SUM(), but for text fields. Concatenates strings.
 cursor.execute("""
-	SELECT
-		I.id, I.dob, I.sex, I.ethnic_code, I.centre_name, I.region_name,
-		I.country_name, I.comments, COALESCE(B.batches, '')
-	FROM "public"."person" I
-	LEFT JOIN (
-		SELECT B.person_id, array_to_string(array_agg(B.batch_id::text), '{0}') AS batches
-		FROM "public"."batches" B
-		GROUP BY B.person_id
-		ORDER BY B.person_id ASC
-	) B ON I.id = B.person_id
-	ORDER BY I.id ASC
+	d
 """.format(utils.SUBARRAY_SEPARATOR))
-print "Writing " + str(cursor.rowcount) + " files\n"
+if importGenotypesFlag != "Y":
+	print "Writing " + str(cursor.rowcount) + " files\n"
+
+
 # This section will need to create the headers and the rows dynamically because
 # the number of columns is not known ahead of time! See utils.py for more info.
 # 8 is where the batches subarray is located
@@ -53,15 +73,65 @@ batchPlaceholders = utils.make_placeholders(subarrayPos, nbBatches)
 headerPlaceholders = utils.make_headers("batch", 0, nbBatches)
 f = cursor.fetchone()
 current_id = -1
+migratedindividuals=0
+
+utils.log(logger, "Writing <<Individuals>> files")
+utils.log(logger, "------------------------------------")
+
 while f:
 	current_id = f[0]
-	os.makedirs(output_dir + str(current_id))
-	with open("{0}{1}/Individuals_{1}.tsv".format(output_dir, current_id), "w") as file:
-		file.write("individualId,familyId,paternalId,maternalId,dateOfBirth,gender,ethnicCode,centreName,region,country,notes,{0}\n".format(headerPlaceholders))
-		li = utils.to_prepared_list(f)
-		li = utils.break_subarray(li, subarrayPos, nbBatches)
-		file.write(("""{0},,,,{1},{2},{3},{4},{5},{6},{7},"""+batchPlaceholders+"""\n""").format(*li))
-		f = cursor.fetchone()
+
+	if importGenotypesFlag == "Y":
+
+ 		regex =  r"^SAM_[0-9]+_{0}_.*{1}$".format(current_id,file_type_oxford)
+                filename =  utils.find_file_in_list(sourcesFileList, regex)
+                familyID = ''
+                fatherID = ''
+		motherID = ''
+
+                if filename == None:
+                        utils.log(logger, "ID: "+str(current_id) +" -Not found in SAMPLE file")
+		else:
+
+			os.makedirs(output_dir + str(current_id))
+
+			with open("{0}{1}/Individuals_{1}.tsv".format(output_dir, current_id), "w") as file:
+				file.write("individualId,familyId,paternalId,maternalId,dateOfBirth,gender,ethnicCode,centreName,region,country,notes,missingCallFreq,{0}\n".format(headerPlaceholders))
+	      		        familyID=utils.get_individual_info(sourcesDirectory+filename, "ID_2")
+		                fatherID=utils.get_individual_info(sourcesDirectory+filename, "father")                    
+		                motherID=utils.get_individual_info(sourcesDirectory+filename, "mother")
+		                sex=utils.get_individual_info(sourcesDirectory+filename, "sex")
+		                missingCallFreq=utils.get_individual_info(sourcesDirectory+filename, "missing")
+				migratedindividuals = migratedindividuals + 1
+			
+				#According to Impute2
+				if sex=='1':
+				    sex="m"
+				elif sex=='2':
+				    sex="f"
+				
+				li = utils.to_prepared_list(f + (familyID,fatherID,motherID,missingCallFreq))
+         			li = utils.break_subarray(li, subarrayPos, nbBatches)	
+
+		                  
+				if sex != li[2]:
+					utils.log(logger, "Difference - Sample SEX: "+sex+ " vs. Advance SEX: "+li[2])
+
+                        	
+				file.write(("""{0},{11},{12},{13},{1},{2},{3},{4},{5},{6},{7},{14},"""+batchPlaceholders+"""\n""").format(*li))
+
+		
+	else:
+		os.makedirs(output_dir + str(current_id))
+		with open("{0}{1}/Individuals_{1}.tsv".format(output_dir, current_id), "w") as file:
+			file.write("individualId,familyId,paternalId,maternalId,dateOfBirth,gender,ethnicCode,centreName,region,country,notes,missingCallFreq,{0}\n".format(headerPlaceholders))
+	      		migratedindividuals = migratedindividuals + 1
+			li = utils.to_prepared_list(f)
+			li = utils.break_subarray(li, subarrayPos, nbBatches)	
+
+			file.write(("""{0},,,,{1},{2},{3},{4},{5},{6},{7},,"""+batchPlaceholders+"""\n""").format(*li))
+			
+	f = cursor.fetchone()
 
 ################################
 ### Individuals_#####_Visits ###
@@ -72,9 +142,14 @@ cursor.execute("""
 	FROM "public"."visit" AS V
 	ORDER BY V.person_id ASC, V.id ASC
 """)
-print "Writing " + str(cursor.rowcount) + " files\n"
+if importGenotypesFlag != "Y":
+	print "Writing " + str(cursor.rowcount) + " files\n"
 f = cursor.fetchone()
 current_id = -1
+
+utils.log(logger, "Writing <<Individuals_Visits>> files")
+utils.log(logger, "------------------------------------")
+
 while f:
 	if f[0] != current_id: # We only close the file when we switch to a new Person
 		try: # Ugly, but Python doesn't let me create ad hoc mocks like JS
@@ -82,17 +157,36 @@ while f:
 		except:
 			pass
 		current_id = f[0]
-		file = open("{0}{1}/Individuals_{1}_Visits.tsv".format(output_dir, current_id), "a")
-		file.write("visitId,visitDate,studyFormId,fasting,description\n")
-	li = utils.to_prepared_list(f)
-	file.write("""{1},{2},{3},{4},{5}\n""".format(*li))
-	f = cursor.fetchone()
+		createdfile=0
 
+		if importGenotypesFlag == "Y":
+
+			regex =  r"^SAM_[0-9]+_{0}_.*{1}$".format(current_id,file_type_oxford)
+		        filename =  utils.find_file_in_list(sourcesFileList, regex)
+	
+			if filename == None:
+                		 utils.log(logger, "ID: "+str(current_id) +" -Not found in SAMPLE file")
+			else:
+		
+				file = open("{0}{1}/Individuals_{1}_Visits.tsv".format(output_dir, current_id), "a")
+				file.write("visitId,visitDate,studyFormId,fasting,description\n")
+				createdfile=1
+	
+		else:
+				file = open("{0}{1}/Individuals_{1}_Visits.tsv".format(output_dir, current_id), "a")
+				file.write("visitId,visitDate,studyFormId,fasting,description\n")
+				createdfile=1
+
+	if createdfile==1:
+		li = utils.to_prepared_list(f)
+		file.write("""{1},{2},{3},{4},{5}\n""".format(*li))
+	
+	f = cursor.fetchone()
 
 ####################################
 ### Individuals_#####_Phenotypes ###
 ####################################
-print "Exportung Individuals Phenotypes"
+print "Exporting Individuals Phenotypes"
 cursor.execute("""
 	SELECT V.person_id, V.id, P.name, M.value, P.um, P.descr, V.visit_date
 	FROM "public"."phenotype" AS P
@@ -103,9 +197,14 @@ cursor.execute("""
 	WHERE V.person_id IS NOT NULL -- There's 1 buggy row, exclude it
 	ORDER BY V.person_id ASC
 """)
-print "Writing " + str(cursor.rowcount) + " files\n"
+if importGenotypesFlag != "Y":
+	print "Writing " + str(cursor.rowcount) + " files\n"
 f = cursor.fetchone()
 current_id = -1
+
+utils.log(logger, "Writing <<Individuals_Phenotypes>> files")
+utils.log(logger, "----------------------------------------")
+
 while f:
 	if f[0] != current_id: # We only close the file when we switch to a new Person
 		try: # Ugly, but Python doesn't let me create ad hoc mocks like JS
@@ -113,10 +212,28 @@ while f:
 		except:
 			pass
 		current_id = f[0]
-		file = open("{0}{1}/Individuals_{1}_Phenotypes.tsv".format(output_dir, current_id), "a")
-		file.write("visitId,phenotypeType,phenotypeGroup,name,measureDataType,measure,units,description,diagnosisDate\n")
-	li = utils.to_prepared_list(f)
-	file.write("""{1},,,{2},,{3},{4},{5},{6}\n""".format(*li))
+		createdfile=0
+
+		if importGenotypesFlag == "Y":
+
+ 			regex =  r"^SAM_[0-9]+_{0}_.*{1}$".format(current_id,file_type_oxford)#check if I I can use IND file
+		        filename =  utils.find_file_in_list(sourcesFileList, regex)
+			if filename == None:
+                        	utils.log(logger, "ID: "+str(current_id) +" -Not found in SAMPLE file")
+			else:
+
+				file = open("{0}{1}/Individuals_{1}_Phenotypes.tsv".format(output_dir, current_id), "a")
+				file.write("visitId,phenotypeType,phenotypeGroup,name,measureDataType,measure,units,description,diagnosisDate\n") 
+				createdfile=1
+		else:
+				file = open("{0}{1}/Individuals_{1}_Phenotypes.tsv".format(output_dir, current_id), "a")
+				file.write("visitId,phenotypeType,phenotypeGroup,name,measureDataType,measure,units,description,diagnosisDate\n") 
+				createdfile=1
+	
+	if createdfile==1:
+		li = utils.to_prepared_list(f)
+		file.write("""{1},,,{2},,{3},{4},{5},{6}\n""".format(*li))
+	
 	f = cursor.fetchone()
 
 
@@ -161,36 +278,109 @@ cursor2.execute("""
 	LEFT JOIN "public"."vcf" V ON A.rs = V.rs
 	ORDER BY A.annotation_version ASC, A.genotype_index ASC
 """)
-print "Writing " + str(cursor1.rowcount) + " files\n"
+if importGenotypesFlag != "Y":
+	print "Writing " + str(cursor1.rowcount) + " files\n"
 f1 = cursor1.fetchone()
 f2 = cursor2.fetchone()
-while f1:
-	current_id = f1[0]
-	current_version = f1[1]
-	filename = "{0}{1}/Individuals_{1}_v{2}_Genotypes.tsv".format(output_dir, current_id, current_version)
-	print "Writing " + filename
-	file = open(filename, "a")
-	file.write("personId,genotypeVersion,sample,letter,rs,chromosome,start,end,ref,alt,allele_a,allele_b\n")
-	buffer = []
-	while f2 and f2[1] == current_version:
-		li1 = utils.to_prepared_list(f1[:3])
-		li2 = utils.to_prepared_list(f2[2:])
-		letter = mapping[f1[3][f2[0]-1]]
-		buffer.append("""{0},{1},{2},{3},{4},{5},{6},{7},{8},"{9}",{10},{11}\n""".format(*(li1+[letter]+li2)))
-		f2 = cursor2.fetchone()
 
-	file.write("".join(buffer))
-	if f2 is None: # rewind
-		cursor2.scroll(0, "absolute")
-		f2 = cursor2.fetchone()
-	file.close()
-	f1 = cursor1.fetchone()
+utils.log(logger, "Writing <<Individuals_Genotypes>> files")
+utils.log(logger, "------------------------------------")
+
+if importGenotypesFlag != "Y": 
+	while f1:
+		current_id = f1[0]
+		current_version = f1[1]
+		filename = "{0}{1}/Individuals_{1}_v{2}_Genotypes.tsv".format(output_dir, current_id, current_version)
+		print "Writing " + filename
+		file = open(filename, "a")
+		file.write("personId,genotypeVersion,sample,letter,rs,chromosome,start,end,ref,alt,allele_a,allele_b,genotypeLikelihoods0,genotypeLikelihoods1,genotypeLikelihoods2\n")
+
+		buffer = []
+		while f2 and f2[1] == current_version:
+			li1 = utils.to_prepared_list(f1[:3])
+			li2 = utils.to_prepared_list(f2[2:])
+			letter = mapping[f1[3][f2[0]-1]]
+			buffer.append("""{0},{1},{2},{3},{4},{5},{6},{7},{8},"{9}",{10},{11},,,\n""".format(*(li1+[letter]+li2)))
+			f2 = cursor2.fetchone()
+
+		file.write("".join(buffer))
+		if f2 is None: # rewind
+			cursor2.scroll(0, "absolute")
+			f2 = cursor2.fetchone()
+
+	
+		file.close()
+		f1 = cursor1.fetchone()
+
+
+#GEN contains new data, different from Advance, I'm creating a new cursor with same Individuals as 
+#indivuals files. It is a complement to the others 2 cursors.-
+
+if importGenotypesFlag == "Y": 
+
+	print " "
+	print "Import Genotypes? : Y..."
+
+	print "Executing SQL query 3..."
+	cursor3 = conn.cursor()
+	cursor3.execute("""
+			SELECT distinct I.id
+			FROM "public"."person" I
+			LEFT JOIN (
+				SELECT B.person_id
+				FROM "public"."batches" B
+				GROUP BY B.person_id
+				ORDER BY B.person_id ASC
+			) B ON I.id = B.person_id
+			ORDER BY I.id ASC
+				 	""")
+	
+	f3 = cursor3.fetchone()
+ 
+	regex =  r"^SNP.*{0}$".format(file_type_oxford)
+	SNPfilename =  utils.find_file_in_list(sourcesFileList, regex)
+
+	if SNPfilename == None:
+  	    utils.log(logger, "SNP file not found")
+	else:
+		while f3:
+			current_id = f3[0]
+			regex =  r"^IND_[0-9]+_{0}_.*{1}$".format(current_id,file_type_oxford)
+
+			INDfilename =  utils.find_file_in_list(sourcesFileList, regex)
+
+			if INDfilename == None:
+			   utils.log(logger, "ID: "+str(current_id) +" -Not found in GEN file")
+			else:
+			   			      	
+
+				filename = "{0}{1}/Individuals_{1}_v{2}_Genotypes.tsv".format(output_dir, current_id, config.imputed_genotype_version)
+				print "Writing " + filename
+
+				INDfile = open(filename, "a")
+
+				INDfile.write("personId,genotypeVersion,sample,letter,rs,chromosome,start,end,ref,alt,allele_a,allele_b,genotypeLikelihoods0,genotypeLikelihoods1,genotypeLikelihoods2\n")
+
+				with open(sourcesDirectory+SNPfilename,'rb') as SFile,open(sourcesDirectory+INDfilename,'rb') as IFile:
+					for x, y in zip(SFile,IFile):
+
+						SNPcolumns=x.split()  
+						INDcolumns=y.split()
+
+		                          	li =  utils.to_prepared_list((str(current_id),config.imputed_genotype_version,SNPcolumns[2],SNPcolumns[3],SNPcolumns[4],str(int(SNPcolumns[4])+len(SNPcolumns[5])),SNPcolumns[5],SNPcolumns[6],INDcolumns[2],INDcolumns[3],INDcolumns[4]))
+						INDfile.write("""{0},{1},,,{3},{2},{4},{5},{6},{7},{6},{7},{8},{9},{10}\n""".format(*li)) 
+  
+				INDfile.close()
+			f3 = cursor3.fetchone()
 
 
 ###############
 ### Batches ###
 ###############
 print "Exporting Batches"
+utils.log(logger, "Writing <<Batches>> file")
+utils.log(logger, "------------------------")
+
 with open(output_dir + "Batches.tsv", "w") as file:
 	file.write("batchId,batchDate,batchType,description,studyName\n")
 
@@ -209,7 +399,9 @@ with open(output_dir + "Batches.tsv", "w") as file:
 		li = utils.to_prepared_list(f)
 		file.write("""S{0},{1},ADVANCE-ON,{2},Enrollment Batch\n""".format(*li)) # Note the "S" before the Sample ID
 		f = cursor.fetchone()
-
+print "-------------------------------"
+print 'Migrated Individuals: '+str(migratedindividuals)
+utils.log(logger, 'Migrated Individuals: '+str(migratedindividuals))
 print "Done"
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import re
 
+#Begin Diego Alvarez
+import logging
+import sys
+import os
+import datetime
+import config
+#End Diego Alvarez
+
 NEWLINE_REPLACEMENT = " " # Default
 SUBARRAY_SEPARATOR = ";" # Default
 FIELD_SEPARATOR = "," # Default
@@ -55,3 +63,82 @@ def make_headers(name, pos, nb):
 # Prepares placeholders (ex: {0}) dynamically to fill in with format()
 def make_placeholders(pos, nb):
 	return FIELD_SEPARATOR.join(["{"+str(i)+"}" for i in xrange(pos, pos+nb)])
+
+#Begin Diego Alvarez
+def create_logger(p_source):
+        # Create a logger
+
+	try:
+        	os.makedirs(config.log_file_destination)
+	except OSError as err:
+	  pass
+	#utils.log(logger,str(err)) 
+
+        now = datetime.datetime.now()
+        LOG_FILENAME=config.log_file_destination+"log_"+p_source+"_"+now.strftime("%Y%m%d_%H%M%S")+".out"
+
+        my_logger = logging.getLogger('MyLogger')
+	my_logger.setLevel(logging.DEBUG)
+
+	# Create file handler
+	fhandler = logging.FileHandler(LOG_FILENAME)
+	fhandler.setLevel(logging.DEBUG)
+
+	# create a logging format
+	#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	formatter = logging.Formatter('%(asctime)s - %(message)s')
+	fhandler.setFormatter(formatter)
+
+	# add the handlers to the logger
+	my_logger.addHandler(fhandler)
+        
+        return my_logger;
+
+def log(p_logger, p_message):
+  	p_logger.debug(p_message)
+
+
+#def find_file_in_list(p_filelist, p_filename):
+#   
+#    filename = None
+#    for item in p_filelist:
+#	if p_filename in item:
+#           filename=item
+#	   break
+#    return filename
+
+def find_file_in_list(p_filelist, p_regex):
+   
+	filename = None
+       
+	for item in p_filelist:
+		if re.match(p_regex, item):
+				filename=item
+				break
+	return filename
+
+
+    
+def get_file_name(p_filename):
+ #Get filename without the extension
+ filename, file_extension = os.path.splitext(p_filename)
+ short_filename=filename.split()
+
+ return short_filename[0]
+
+def get_individual_info(p_filename, p_info):
+   
+   info= None
+
+   with open(p_filename,'rb') as f:
+	
+     for line in f:
+	  columns=line.split()       	 
+          if columns[0] == p_info:
+             info =  columns[2]
+             break
+   return info
+
+  
+
+#End Diego Alvarez
